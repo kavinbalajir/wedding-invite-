cafadc224232acae557e59fc529161a5cb8c6ed1
kavinbalajir <kavinbalaji.2513@gmail.com>
2026-08-14 20:36:07 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wedding-invite/',
+  base: '/wedding-invite-/',
   plugins: [react()],
 })
